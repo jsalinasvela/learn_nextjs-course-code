3f17de4d755a34aa6c6eb19ae0e0f24196bbdc9b
Jesus Salinas <jjesus.salinas@gmail.com>
2023-01-10 20:48:15 -0500

    Lesson 83: Navigating to the "Filtered Events" page programatically

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,21 @@
+import { useRouter } from "next/router";
+
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
 
 function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
     
