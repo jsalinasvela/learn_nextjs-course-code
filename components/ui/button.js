d4365bf8624f01e54dd2b801f399be65ede73c51
Jesus Salinas <jjesus.salinas@gmail.com>
2023-01-10 20:34:52 -0500

    Lesson 82: Adding a Filter Form for filtering events

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,12 +2,22 @@ import Link from "next/link";
 import classes from "./button.module.css";
 
 function Button(props) {
-  const { link, children } = props;
+  const { link, children, onClick } = props;
 
   return (
-    <Link className={classes.btn} href={link}>
-      {children}
-    </Link>
+    <>
+      {
+        link ? (
+          <Link className={classes.btn} href={link}>
+            {children}
+          </Link>
+        ): (
+          <button className={classes.btn} onClick={onClick}>
+            {children}
+          </button>
+        )
+      }
+    </>
   );
 }
 
