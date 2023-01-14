608ce9b347d475439ddfd952b2552b2cf472311d
Jesus Salinas <jjesus.salinas@gmail.com>
2023-01-14 11:50:01 -0500

    93,94: Adding getStaticProps to Pages and Running Server-side Code & Using the Filesystem

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,39 @@
+import path from 'path';
+import fs from 'fs/promises';
+
 function HomePage(props) {
+  console.log('props.products', props.products);
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {props.products.map(product => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+
+  // const products = await fetch('https://nextjs-course-4f3e2-default-rtdb.firebaseio.com/products.json').then(res => res.json());
+  // const transformedProducts = Object.keys(products).map(key => ({ id: key, ...products[key] }));
+  // return {
+  //   props: {
+  //     products: transformedProducts
+  //   }
+  // }
+
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  console.log('data', data);
+
+  return {
+    props: {
+      products: data.products
+    }
+  }
 }
 
 export default HomePage;
