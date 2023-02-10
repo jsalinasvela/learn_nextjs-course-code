ef8577cc1903590c0af6499484f5a70b80876d75
Jesus Salinas <jjesus.salinas@gmail.com>
2023-02-09 19:41:49 -0500

    Bring data from Firebase database: Section 6: 121 - Loading Data & Paths for Dynamic Pages

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
-import { getFeaturedEvents } from '../dummy-data';
+import { getAllEvents, getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
+  const { featuredEvents } = props;
+
+  if (!featuredEvents) return <p>No events</p>;
 
   return (
     <div>
       <EventList items={featuredEvents} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      featuredEvents,
+    },
+    revalidate: 1800,
+  };
 }
 
 export default HomePage;
